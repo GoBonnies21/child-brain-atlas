01eaaf45fdbbf103d27291c203e2201d0b2a8575
GoBonnies21 <60935201+GoBonnies21@users.noreply.github.com>
2020-03-28 20:31:54 -0400

    Added an arry that can have text added to it and the fx to do so.

--- App.js
+++ App.js
@@ -2,13 +2,33 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import BrainButton from './components/button'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <BrainButton showOnButton="test"></BrainButton>
-    </View>
-  );
+export default class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      infoAboutArea: []
+    }
+    this.showInfoAboutArea = this.showInfoAboutArea.bind(this)
+  }
+  showInfoAboutArea(info){
+    this.setState({
+      infoAboutArea:[info]
+    })
+  }
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>The text below this changes!</Text>
+    <Text>{this.state.infoAboutArea}</Text>
+        <BrainButton 
+        showOnButton="The Brain"
+        info = "The brain tells your body what to do"
+        showInfoAboutArea = {this.showInfoAboutArea}
+        ></BrainButton>
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
--- components/button.js
+++ components/button.js
@@ -3,11 +3,18 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class BrainButton extends React.Component {
     constructor(props){
-        super(props)
+        super(props);
+        this.showInfoHandler= this.showInfoHandler.bind(this);
+    }
+    showInfoHandler(){
+        this.props.showInfoAboutArea(this.props.info)
     }
     render(){
         return (
-            <Button title={this.props.showOnButton}></Button>
+            <Button 
+            title={this.props.showOnButton}
+            onPress = {this.showInfoHandler}
+            ></Button>
         )
     }
 }
