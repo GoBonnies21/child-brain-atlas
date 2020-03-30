687d1a24ba5391253b3e50ce8fccf95c8978c51b
GoBonnies21 <60935201+GoBonnies21@users.noreply.github.com>
2020-03-30 14:40:45 -0400

    Made it pretty with Prettier.

--- App.js
+++ App.js
@@ -1,46 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import BrainButton from './components/button'
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import BrainButton from "./components/button";
 
-export default class App extends React.Component{
-  constructor(){
-    super()
+export default class App extends React.Component {
+  constructor() {
+    super();
     this.state = {
       infoAboutArea: []
-    }
-    this.showInfoAboutArea = this.showInfoAboutArea.bind(this)
+    };
+    this.showInfoAboutArea = this.showInfoAboutArea.bind(this);
   }
-  showInfoAboutArea(info){
+  showInfoAboutArea(info) {
     this.setState({
-      infoAboutArea:[info]
-    })
+      infoAboutArea: [info]
+    });
   }
-  render(){
+  render() {
     return (
       <View style={styles.container}>
         <Text>The text below this changes!</Text>
-    <Text>{this.state.infoAboutArea}</Text>
-        <BrainButton 
-        showOnButton="The Brain"
-        info = "The brain tells your body what to do"
-        showInfoAboutArea = {this.showInfoAboutArea}
+        <Text>{this.state.infoAboutArea}</Text>
+        <BrainButton
+          showOnButton="The Brain"
+          info="The brain tells your body what to do"
+          showInfoAboutArea={this.showInfoAboutArea}
         ></BrainButton>
-        <BrainButton 
-        showOnButton="The frontal lobe"
-        info = "The frontal lobe is .... "
-        showInfoAboutArea = {this.showInfoAboutArea}
+        <BrainButton
+          showOnButton="The frontal lobe"
+          info="The frontal lobe is .... "
+          showInfoAboutArea={this.showInfoAboutArea}
         ></BrainButton>
       </View>
     );
   }
-  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
