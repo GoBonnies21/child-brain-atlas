0bf6d000c0ed0d3ad796a92b31050d5e6616575c
GoBonnies21 <60935201+GoBonnies21@users.noreply.github.com>
2020-04-09 21:38:29 -0400

    Initiated Navigation, pulled the Interior to a new .js file, implimented navigation, general file cleanup.

--- App.js
+++ App.js
@@ -1,68 +1,50 @@
+/* eslint-disable func-style */
+import "react-native-gesture-handler";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import BrainButton from "./components/button";
-import BrainImage from "./components/images";
+import { StyleSheet, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import BrainExterior from "./brainExterior";
+import BrainInterior from "./brainInterior";
 
+// eslint-disable-next-line react/prop-types
+function brainExteriorFx({ navigation }) {
+  return (
+    <View>
+      <BrainExterior></BrainExterior>
+      <Button
+        title="Flip the Brain"
+        // eslint-disable-next-line react/prop-types
+        onPress={() => navigation.navigate("interior")}
+      />
+    </View>
+  );
+}
+
+function brainInteriorFx() {
+  return <BrainInterior></BrainInterior>;
+}
+const Stack = createStackNavigator();
 export default class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      infoAboutArea: []
-    };
-    this.showInfoAboutArea = this.showInfoAboutArea.bind(this);
-  }
-  showInfoAboutArea(info) {
-    this.setState({
-      infoAboutArea: [info]
-    });
   }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>{this.state.infoAboutArea}</Text>
-        <BrainButton
-          showOnButton="The Brain"
-          info="The brain tells your body what to do"
-          showInfoAboutArea={this.showInfoAboutArea}
-        ></BrainButton>
-        <BrainButton
-          showOnButton="The Frontal lobe"
-          info='The frontal lobe is the "thinking" part of the brain. It gives us our emotions and lets us solve problems '
-          showInfoAboutArea={this.showInfoAboutArea}
-        ></BrainButton>
-        <BrainButton
-          showOnButton="The Parietal Lobe"
-          info="The parietal lobe takes our different senses and makes connections between them. This lets us do things like catch a baseball or tell what room of the house our dog is barking in"
-          showInfoAboutArea={this.showInfoAboutArea}
-        ></BrainButton>
-        <BrainButton
-          showOnButton="The Occipital Lobe"
-          info="The occipital lobe helps us see. Can you believe all that information travels from your eyes to the back of your head?"
-          showInfoAboutArea={this.showInfoAboutArea}
-        ></BrainButton>
-        <BrainButton
-          showOnButton="The Temporal Lobe"
-          info="The temporal lobe allows us to hear sounds, understand words and remember pictures and faces."
-          showInfoAboutArea={this.showInfoAboutArea}
-        ></BrainButton>
-        <BrainButton
-          showOnButton="The Cerebellum"
-          info="The cerebellum is responsible for keeping us balance. It lets us dance, play sports or carry glasses full of water or juice."
-          showInfoAboutArea={this.showInfoAboutArea}
-        ></BrainButton>
-        <BrainButton
-          showOnButton="The Brain Stem"
-          info="The brain stem keeps our heart pumping and tells our lungs to breathe."
-          showInfoAboutArea={this.showInfoAboutArea}
-        ></BrainButton>
-        <BrainButton
-          showOnButton="The Spinal Cord"
-          info="The spinal cord takes all of the instructions from our brain and brings them to the rest of our body. It also takes all the things we feel with our skin and tells our brain about it."
-          showInfoAboutArea={this.showInfoAboutArea}
-        ></BrainButton>
-        <BrainImage>
-        </BrainImage>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="exterior">
+          <Stack.Screen
+            name="exterior"
+            component={brainExteriorFx}
+            options={{ title: "The Exterior of the Brain" }}
+          />
+          <Stack.Screen
+            name="interior"
+            component={brainInteriorFx}
+            options={{ title: "The Interior of the Brain" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
@@ -72,6 +54,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
-  }
+    justifyContent: "center",
+  },
 });
---
+++ brainExterior.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import BrainButton from "./components/button";
+import BrainImage from "./components/images";
+export default class BrainExterior extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+          infoAboutArea: []
+        };
+        this.showInfoAboutArea = this.showInfoAboutArea.bind(this);
+      }
+      showInfoAboutArea(info) {
+        this.setState({
+          infoAboutArea: [info]
+        });
+      }
+      render() {
+        return (
+          <View style={styles.container}>
+            <Text>{this.state.infoAboutArea}</Text>
+            <BrainButton
+              showOnButton="The Brain"
+              info="The brain tells your body what to do"
+              showInfoAboutArea={this.showInfoAboutArea}
+            ></BrainButton>
+            <BrainButton
+              showOnButton="The Frontal lobe"
+              info='The frontal lobe is the "thinking" part of the brain. It gives us our emotions and lets us solve problems '
+              showInfoAboutArea={this.showInfoAboutArea}
+            ></BrainButton>
+            <BrainButton
+              showOnButton="The Parietal Lobe"
+              info="The parietal lobe takes our different senses and makes connections between them. This lets us do things like catch a baseball or tell what room of the house our dog is barking in"
+              showInfoAboutArea={this.showInfoAboutArea}
+            ></BrainButton>
+            <BrainButton
+              showOnButton="The Occipital Lobe"
+              info="The occipital lobe helps us see. Can you believe all that information travels from your eyes to the back of your head?"
+              showInfoAboutArea={this.showInfoAboutArea}
+            ></BrainButton>
+            <BrainButton
+              showOnButton="The Temporal Lobe"
+              info="The temporal lobe allows us to hear sounds, understand words and remember pictures and faces."
+              showInfoAboutArea={this.showInfoAboutArea}
+            ></BrainButton>
+            <BrainButton
+              showOnButton="The Cerebellum"
+              info="The cerebellum is responsible for keeping us balance. It lets us dance, play sports or carry glasses full of water or juice."
+              showInfoAboutArea={this.showInfoAboutArea}
+            ></BrainButton>
+            <BrainButton
+              showOnButton="The Brain Stem"
+              info="The brain stem keeps our heart pumping and tells our lungs to breathe."
+              showInfoAboutArea={this.showInfoAboutArea}
+            ></BrainButton>
+            <BrainButton
+              showOnButton="The Spinal Cord"
+              info="The spinal cord takes all of the instructions from our brain and brings them to the rest of our body. It also takes all the things we feel with our skin and tells our brain about it."
+              showInfoAboutArea={this.showInfoAboutArea}
+            ></BrainButton>
+            <BrainImage>
+            </BrainImage>
+          </View>
+        );
+      }
+    }
+    const styles = StyleSheet.create({
+        container: {
+          flex: 1,
+          backgroundColor: "#fff",
+          alignItems: "center",
+          justifyContent: "center"
+        }
+      });
