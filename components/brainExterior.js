d29c16c9013bcd985920264241a580bbf2d858ad
GoBonnies21 <60935201+GoBonnies21@users.noreply.github.com>
2020-05-01 13:53:00 -0400

    Buttons now scale to image size.

--- components/brainExterior.js
+++ components/brainExterior.js
@@ -4,69 +4,69 @@ import BrainButton from "./button";
 import ExteriorBrainImage from "./OuterBrainImage";
 import styles from "./styles";
 export default class BrainExterior extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-          infoAboutArea: []
-        };
-        this.showInfoAboutArea = this.showInfoAboutArea.bind(this);
-      }
-      showInfoAboutArea(info) {
-        this.setState({
-          infoAboutArea: [info]
-        });
-      }
-      render() {
-        return (
-          <View style={styles.BrainExteriorContainer}>
-            <Text style={styles.message}>{this.state.infoAboutArea}</Text>
-            <View style={styles.backgroundImage}>
-            <ExteriorBrainImage></ExteriorBrainImage>
-            </View>
-            <View style={styles.Brain}>
+  constructor() {
+    super();
+    this.state = {
+      infoAboutArea: [],
+    };
+    this.showInfoAboutArea = this.showInfoAboutArea.bind(this);
+  }
+  showInfoAboutArea(info) {
+    this.setState({
+      infoAboutArea: [info],
+    });
+  }
+  render() {
+    return (
+      <View style={styles.BrainExteriorContainer}>
+        <Text style={styles.message}>{this.state.infoAboutArea}</Text>
+        <View style={styles.backgroundImage}>
+          <ExteriorBrainImage></ExteriorBrainImage>
+
+          <View style={styles.Brain}>
             <BrainButton
               showOnButton="The Brain"
               info="The brain tells your body what to do"
               showInfoAboutArea={this.showInfoAboutArea}
             ></BrainButton>
-            </View>
-            <View style={styles.FrontalLobe}>
+          </View>
+          <View style={styles.FrontalLobe}>
             <BrainButton
               showOnButton="The Frontal lobe"
               info='The frontal lobe is the "thinking" part of the brain. It gives us our emotions and lets us solve problems '
               showInfoAboutArea={this.showInfoAboutArea}
             ></BrainButton>
-            </View>
-            <View style={styles.ParietalLobe}>
+          </View>
+          <View style={styles.ParietalLobe}>
             <BrainButton
               showOnButton="The Parietal Lobe"
               info="The parietal lobe takes our different senses and makes connections between them. This lets us do things like catch a baseball or tell what room of the house our dog is barking in"
               showInfoAboutArea={this.showInfoAboutArea}
             ></BrainButton>
-            </View>
-            <View style={styles.OccipitalLobe}>
+          </View>
+          <View style={styles.OccipitalLobe}>
             <BrainButton
               showOnButton="The Occipital Lobe"
               info="The occipital lobe helps us see. Can you believe all that information travels from your eyes to the back of your head?"
               showInfoAboutArea={this.showInfoAboutArea}
             ></BrainButton>
-            </View>
-            <View style={styles.TemporalLobe}>
+          </View>
+          <View style={styles.TemporalLobe}>
             <BrainButton
               showOnButton="The Temporal Lobe"
               info="The temporal lobe allows us to hear sounds, understand words and remember pictures and faces."
               showInfoAboutArea={this.showInfoAboutArea}
             ></BrainButton>
-            </View>
-            <View style={styles.Cerebellum}>
+          </View>
+          <View style={styles.Cerebellum}>
             <BrainButton
               showOnButton="The Cerebellum"
               info="The cerebellum is responsible for keeping us balance. It lets us dance, play sports or carry glasses full of water or juice."
               showInfoAboutArea={this.showInfoAboutArea}
             ></BrainButton>
-            </View>
           </View>
-        );
-      }
-    }
-    
+        </View>
+      </View>
+    );
+  }
+}
--- components/neuronPage.js
+++ components/neuronPage.js
@@ -22,62 +22,63 @@ export default class NeuronPage extends React.Component {
         <Text style={styles.message}>{this.state.infoAboutArea}</Text>
         <View style={styles.backgroundImage}>
           <NeuronImage></NeuronImage>
-        </View>
-        <View style={styles.CellBody}>
-          <BrainButton
-            showOnButton="The Cell Body"
-            info="The cell body contains all the pieces the neuron needs to survive"
-            showInfoAboutArea={this.showInfoAboutArea}
-          ></BrainButton>
-        </View>
-        <View style={styles.Dendrite}>
-          <BrainButton
-            showOnButton="The Dendrites"
-            info="The dendrites receive information from other neurons and help relay messages"
-            showInfoAboutArea={this.showInfoAboutArea}
-          ></BrainButton>
-        </View>
-        <View style={styles.Axon}>
-          <BrainButton
-            showOnButton="The Axon"
-            info="The axon is what allows a neuron to relay its messages to other neurons.  Electrical signals travel along it!"
-            showInfoAboutArea={this.showInfoAboutArea}
-          ></BrainButton>
-        </View>
-        <View style={styles.NodeofRanvier}>
-          <BrainButton
-            showOnButton="Node of Ranvier"
-            info="A node of Ranvier allows the signal to keep moving.  It is a gap in the myelin sheath on the axon"
-            showInfoAboutArea={this.showInfoAboutArea}
-          ></BrainButton>
-        </View>
-        <View style={styles.MyelinSheath}>
-          <BrainButton
-            showOnButton="The Myelin Sheath"
-            info="The myelin sheath insulates the axon and allows signals to move quickly down it"
-            showInfoAboutArea={this.showInfoAboutArea}
-          ></BrainButton>
-        </View>
-        <View style={styles.Synapse}>
-          <BrainButton
-            showOnButton="The Synapse"
-            info="The synapse is the junction between two neurons where messages will be transferred using neurotransmitters, a special type of chemical"
-            showInfoAboutArea={this.showInfoAboutArea}
-          ></BrainButton>
-        </View>
-        <View style={styles.SchwannCell}>
-          <BrainButton
-            showOnButton="The Schwann Cells"
-            info="The Schwann cells surround the axon and provide the myelin for the sheath"
-            showInfoAboutArea={this.showInfoAboutArea}
-          ></BrainButton>
-        </View>
-        <View style={styles.AxonHillock}>
-          <BrainButton
-            showOnButton="The Axon Hillock"
-            info="The axon hillock joins the cell body and the axon so messages can keep moving"
-            showInfoAboutArea={this.showInfoAboutArea}
-          ></BrainButton>
+
+          <View style={styles.CellBody}>
+            <BrainButton
+              showOnButton="The Cell Body"
+              info="The cell body contains all the pieces the neuron needs to survive"
+              showInfoAboutArea={this.showInfoAboutArea}
+            ></BrainButton>
+          </View>
+          <View style={styles.Dendrite}>
+            <BrainButton
+              showOnButton="The Dendrites"
+              info="The dendrites receive information from other neurons and help relay messages"
+              showInfoAboutArea={this.showInfoAboutArea}
+            ></BrainButton>
+          </View>
+          <View style={styles.Axon}>
+            <BrainButton
+              showOnButton="The Axon"
+              info="The axon is what allows a neuron to relay its messages to other neurons.  Electrical signals travel along it!"
+              showInfoAboutArea={this.showInfoAboutArea}
+            ></BrainButton>
+          </View>
+          <View style={styles.NodeofRanvier}>
+            <BrainButton
+              showOnButton="Node of Ranvier"
+              info="A node of Ranvier allows the signal to keep moving.  It is a gap in the myelin sheath on the axon"
+              showInfoAboutArea={this.showInfoAboutArea}
+            ></BrainButton>
+          </View>
+          <View style={styles.MyelinSheath}>
+            <BrainButton
+              showOnButton="The Myelin Sheath"
+              info="The myelin sheath insulates the axon and allows signals to move quickly down it"
+              showInfoAboutArea={this.showInfoAboutArea}
+            ></BrainButton>
+          </View>
+          <View style={styles.Synapse}>
+            <BrainButton
+              showOnButton="The Synapse"
+              info="The synapse is the junction between two neurons where messages will be transferred using neurotransmitters, a special type of chemical"
+              showInfoAboutArea={this.showInfoAboutArea}
+            ></BrainButton>
+          </View>
+          <View style={styles.SchwannCell}>
+            <BrainButton
+              showOnButton="The Schwann Cells"
+              info="The Schwann cells surround the axon and provide the myelin for the sheath"
+              showInfoAboutArea={this.showInfoAboutArea}
+            ></BrainButton>
+          </View>
+          <View style={styles.AxonHillock}>
+            <BrainButton
+              showOnButton="The Axon Hillock"
+              info="The axon hillock joins the cell body and the axon so messages can keep moving"
+              showInfoAboutArea={this.showInfoAboutArea}
+            ></BrainButton>
+          </View>
         </View>
       </View>
     );
